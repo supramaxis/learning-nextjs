b221b2cd9c80a110a7c20b624bc5e672f9d5ca8e
supramaxis <kkssccd@gmail.com>
2023-04-22 03:47:49 +0000

    pokemon components

---
+++ src/components/PokeAPI/PokeInfo.jsx
@@ -0,0 +1,15 @@
+export default function PokeInfo({ data }) {
+  return (
+    <>
+      {!data ? (
+        ''
+      ) : (
+        <div>
+          <h2>Name: {data.name}</h2>
+          <img src={data.sprites.front_default} />
+        </div>
+      )}
+    </>
+  );
+}
+
