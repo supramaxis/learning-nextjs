e9c89fc89009c0e9a6396abff219c8115c385370
supramaxis <kkssccd@gmail.com>
2023-05-18 08:12:26 +0000

    prisma files

---
+++ prisma/migrations/20230516112551_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Url" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "shortUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Url_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Url_url_key" ON "Url"("url");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Url_shortUrl_key" ON "Url"("shortUrl");
---
+++ prisma/migrations/20230516114408_init/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "Url_url_key";
