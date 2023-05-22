1b26ef04f6fee48304df79cf847edbb55a7f4539
supramaxis <kkssccd@gmail.com>
2023-05-22 00:11:08 +0000

    chore: modal closing after clicking create

--- src/app/shorten/page.tsx
+++ src/app/shorten/page.tsx
@@ -7,20 +7,42 @@ import {
   FormControl,
   FormHelperText,
   FormLabel,
-  Input
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useState, useRef, useEffect } from 'react';
 import getSession from '@/app/actions/getSession';
-import { Session } from 'next-auth';
+import { useSession } from 'next-auth/react';
+
+import { toast } from 'react-hot-toast';
 
 export default function Shorten() {
-  const [userUrls, setUserUrls] = useState([]);
+  const [someUserUrls, setSomeUserUrls] = useState([]);
   const [shortUrl, setShortUrl] = useState('');
   const [loading, setLoading] = useState(false);
-  const [session, setSession] = useState<Session | null>(null);
+  const { data: session, status } = useSession();
   const inputRef = useRef<HTMLInputElement>(null);
   const customCodeRef = useRef<HTMLInputElement>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = useRef(null);
+  const finalRef = useRef(null);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -30,6 +52,7 @@ export default function Shorten() {
     const customCode = customCodeRef.current?.value || undefined;
 
     //fetch the api endpoint to create a short url using async await
+    setLoading(true);
     const res = await fetch('/api/shorten', {
       method: 'POST',
       headers: {
@@ -39,34 +62,122 @@ export default function Shorten() {
     });
     const data = await res.json();
     setShortUrl(data.shortUrl);
+    setLoading(false);
 
-    if (loading) return <p>Loading...</p>;
+    onClose();
+
+    fetchSessionAndLinks();
   };
+
+  useEffect(() => {
+    if (!isOpen) {
+      setShortUrl('');
+      if (inputRef.current) inputRef.current.value = '';
+      if (customCodeRef.current) customCodeRef.current.value = '';
+    }
+  }, [isOpen]);
+
+  const fetchSessionAndLinks = async () => {
+    // Solo ejecutar en el entorno del servidor
+    if (session) {
+      const res = await fetch('/api/urls', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await res.json();
+      console.log('Your data', data);
+      setSomeUserUrls(data);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchSessionAndLinks();
+  }, [session]);
+
   return (
     <>
-      <Center h='100vh' w='100vw'>
-        <Container>
-          <FormControl onSubmit={handleSubmit}>
-            <FormLabel>Ingresa la URL a acortar</FormLabel>
-            <Input type='url' ref={inputRef} required />
-            <FormLabel>Código personalizado (opcional)</FormLabel>
-            <Input type='text' ref={customCodeRef} />
-            <Button onClick={handleClick} type='submit' mt={4}>
+      <Center h='100px'>
+        <Button onClick={onOpen}>Open Modal</Button>
+      </Center>
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        isCentered
+        onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Acorta tus links</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl onSubmit={handleSubmit}>
+              {/* {loading && <Center>Loading...</Center>} */}
+
+              <FormLabel>Ingresa la URL a acortar</FormLabel>
+              <Input type='url' ref={inputRef} required />
+              <FormLabel>Código personalizado (opcional)</FormLabel>
+              <Input type='text' ref={customCodeRef} />
+
+              {shortUrl && (
+                <>
+                  <FormHelperText>URL acortada con éxito</FormHelperText>
+                  <FormHelperText>
+                    <Link target='_blank' href={`/go/${shortUrl}`}>
+                      http://localhost:3000/go/{shortUrl}
+                    </Link>
+                  </FormHelperText>
+                </>
+              )}
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              onClick={handleClick}
+              type='submit'
+              colorScheme='blue'
+              mr={3}>
               Acortar
             </Button>
-            {shortUrl && (
-              <>
-                <FormHelperText>URL acortada con éxito</FormHelperText>
-                <FormHelperText>
-                  <Link target='_blank' href={`/go/${shortUrl}`}>
-                    http://localhost:3000/go/{shortUrl}
-                  </Link>
-                </FormHelperText>
-              </>
-            )}
-          </FormControl>
-        </Container>
-      </Center>
+            <Button onClick={onClose}>Cerrar</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      {/* {loading && <Center>Loading...</Center>} */}
+      {someUserUrls.length === 0 ? (
+        <Center h='100px'>No hay links para mostrar. Crea algunos</Center>
+      ) : (
+        <TableContainer>
+          <Table variant='simple' size={'lg'}>
+            <TableCaption>
+              Hola {session?.user.name} Estos son tus enlaces
+            </TableCaption>
+            <Thead>
+              <Tr>
+                <Th isNumeric>Link number</Th>
+                <Th>ShortUrl</Th>
+                <Th>Created At</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {someUserUrls.map((urlObj: any) => (
+                <Tr key={urlObj.id}>
+                  <Td>{urlObj.id}</Td>
+                  <Td>
+                    <Link href={`/go/${urlObj.shortUrl}`} target='_blank'>
+                      http://localhost:3000/go/{urlObj.shortUrl}
+                    </Link>
+                  </Td>
+                  <Td>{urlObj.createdAt}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      )}
     </>
   );
 }
