158a7e01ca4de418025cc988b7cf63c266ce2ecc
supramaxis <kkssccd@gmail.com>
2023-05-23 05:50:54 +0000

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,23 @@
 import { withAuth } from 'next-auth/middleware';
+export { default } from 'next-auth/middleware';
 
-export default withAuth({
-  pages: {
-    signIn: '/'
-  }
-});
+// export default withAuth({
+//   pages: {
+//     signIn: '/login'
+//   },
+
+//   callbacks: {
+//     authorized: ({ req, token }) => {
+//       // Allow access to the root ("/") route without authentication
+//       if (req.nextUrl.pathname === '/login') return true;
+
+//       // Protect all other routes by allowing access only to authenticated users
+//       return !!token;
+//     }
+//   }
+// });
 
 export const config = {
-  matcher: ['/users/:path*']
+  matcher: ['/((?!register|api|login).*)']
 };
 
