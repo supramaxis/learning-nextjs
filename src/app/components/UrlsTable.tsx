703e12d715e338632cf0238d719a5c5c63eb8b6a
supramaxis <kkssccd@gmail.com>
2023-05-24 11:13:55 +0000

    moved interface and fixed links not rendering

--- src/app/components/UrlsTable.tsx
+++ src/app/components/UrlsTable.tsx
@@ -16,54 +16,61 @@ import Link from 'next/link';
 import UrlsContext from '@/context/UrlsContext';
 import { useContext } from 'react';
 import { useSession } from 'next-auth/react';
-import useSWR from 'swr';
+import { mutate } from 'swr';
+import DropDown from './DropDown';
+import { DataItem, UrlsTableProps } from '@/types';
 
-interface DataItem {
-  id: number;
-  url: string;
-  shortUrl: string;
-  customCode: string | null;
-  createdAt: string;
-  userId: string;
-}
-
-const UrlsTable: React.FC = () => {
-  const { data, error } = useContext(UrlsContext);
-  // const { data, error } = useSWR<DataItem[]>('/api/urls', fetcher);
+const UrlsTable: React.FC<UrlsTableProps> = ({ data }) => {
+  const { data: contextData, error } = useContext(UrlsContext);
   const { data: session } = useSession();
 
   if (error) return <Center>loading ...</Center>;
-  if (!data) return <Center>loading...</Center>;
+  if (!contextData) return <Center>loading...</Center>;
+
+  const handleUrlDeleted = (deletedId: number) => {
+    const updatedData = contextData.filter(
+      (urlObj: DataItem) => urlObj.id !== deletedId
+    );
+    mutate(updatedData);
+  };
 
   return (
-    <TableContainer>
-      <Table variant='simple' size={'md'}>
-        <TableCaption>
-          Hola {session?.user.name} estos son tus links
-        </TableCaption>
+    <div className='flex items-center justify-center h-full '>
+      <TableContainer className='mx-auto'>
+        <Table variant='simple' size={'md'} className='w-full'>
+          <TableCaption>
+            Hola {session?.user.name} estos son tus links
+          </TableCaption>
 
-        <Thead>
-          <Tr>
-            <Th isNumeric>Link number</Th>
-            <Th>ShortUrl</Th>
-            <Th>Created At</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.map((urlObj: DataItem) => (
-            <Tr key={urlObj.id}>
-              <Td>{urlObj.id}</Td>
-              <Td>
-                <Link href={`/go/${urlObj.shortUrl}`} target='_blank'>
-                  http://localhost:3000/go/{urlObj.shortUrl}
-                </Link>
-              </Td>
-              <Td>{urlObj.createdAt}</Td>
+          <Thead className='block gap-10'>
+            <Tr className='gap-10'>
+              <Th isNumeric>Link ID</Th>
+              <Th>ShortUrl</Th>
+              <Th>Created At</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+          </Thead>
+          <Tbody className='block h-52 overflow-y-auto w-full'>
+            {data.map((urlObj: DataItem) => (
+              <Tr key={urlObj.id}>
+                <Td>{urlObj.id}</Td>
+                <Td>
+                  <Link href={`/go/${urlObj.shortUrl}`} target='_blank'>
+                    http://localhost:3000/go/{urlObj.shortUrl}
+                  </Link>
+                </Td>
+                <Td>{urlObj.createdAt}</Td>
+                <Td>
+                  <DropDown
+                    id={urlObj.id}
+                    onUrlDeleted={() => handleUrlDeleted(urlObj.id)}
+                  />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
