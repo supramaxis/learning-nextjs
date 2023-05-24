46a54a63553d431c6a53a1ba7d9d5119986701e8
supramaxis <kkssccd@gmail.com>
2023-05-24 11:14:17 +0000

    fixed links not being deleted from web ui

--- src/app/components/DropDown.tsx
+++ src/app/components/DropDown.tsx
@@ -8,8 +8,33 @@ import {
   IconButton
 } from '@chakra-ui/react';
 import { HamburgerIcon, DeleteIcon, EditIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
+import axios from 'axios';
 
-const DropDown = () => {
+interface DropDownProps {
+  id: number;
+  onUrlDeleted: () => void;
+}
+
+const DropDown: React.FC<DropDownProps> = (
+  { id }: { id: number },
+  onUrlDeleted
+) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleDelete = async () => {
+    setLoading(true);
+
+    try {
+      const res = await axios.delete(`/api/delete/${id}`);
+      console.log(res.data);
+      onUrlDeleted();
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
   return (
     <Menu isLazy>
       <MenuButton
@@ -20,7 +45,9 @@ const DropDown = () => {
       />
       <MenuList>
         <MenuItem icon={<EditIcon />}>Editar</MenuItem>
-        <MenuItem icon={<DeleteIcon />}>Eliminar</MenuItem>
+        <MenuItem onClick={handleDelete} icon={<DeleteIcon />}>
+          Eliminar
+        </MenuItem>
       </MenuList>
     </Menu>
   );
