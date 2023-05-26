9a956e9ccb9b1a86dc06d52feb3c73d40bbd10b2
supramaxis <kkssccd@gmail.com>
2023-05-26 23:53:00 +0000

    added some utils anf layout components

---
+++ src/app/components/ResizableHandle.tsx
@@ -0,0 +1,28 @@
+import { Box } from '@chakra-ui/react';
+import { useRef } from 'react';
+
+type ResizableHandleProps = {
+  column: any;
+  onDrag: (e: MouseEvent) => void;
+};
+
+const ResizableHandle: React.FC<ResizableHandleProps> = ({
+  column,
+  onDrag
+}) => {
+  const handleRef = useRef<HTMLDivElement>(null);
+  const handleMouseDown = (e: React.MouseEvent) => {
+    onDrag(e.nativeEvent);
+  };
+
+  return (
+    <div
+      ref={handleRef}
+      className='absolute left-0 top-0 bottom-0 cursor-col-resize'
+      onMouseDown={handleMouseDown}
+      style={{ left: `${column.width}px`, zIndex: 100 }}></div>
+  );
+};
+
+export default ResizableHandle;
+
---
+++ src/app/hooks/useTimeAgoLabel.tsx
@@ -0,0 +1,49 @@
+// useTimeAgoLabel.ts - Custom hook for displaying a time ago label
+
+import { useEffect, useState } from 'react';
+
+const getTimeAgoLabel = (dateString: string, currentTime: Date) => {
+  if (!dateString || !currentTime) {
+    return '';
+  }
+  const now = new Date();
+  const date = new Date(dateString);
+  const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} seconds ago`;
+  } else if (diffInSeconds < 3600) {
+    const minutes = Math.floor(diffInSeconds / 60);
+    return `${minutes} minutes ago`;
+  } else if (diffInSeconds < 86400) {
+    const hours = Math.floor(diffInSeconds / 3600);
+    return `${hours} hours ago`;
+  } else if (diffInSeconds < 2592000) {
+    const days = Math.floor(diffInSeconds / 86400);
+    return `${days} days ago`;
+  } else if (diffInSeconds < 31536000) {
+    const months = Math.floor(diffInSeconds / 2592000);
+    return `${months} months ago`;
+  }
+  return '';
+};
+
+export const useTimeAgoLabel = (dateString: string, currentTime: Date) => {
+  const [timeAgoLabel, setTimeAgoLabel] = useState('');
+
+  useEffect(() => {
+    const updateTimeAgo = () => {
+      const currentTime = new Date();
+      const timeAgo = getTimeAgoLabel(dateString, currentTime);
+      setTimeAgoLabel(timeAgo);
+
+      const interval = setInterval(updateTimeAgo, 1000);
+      updateTimeAgo();
+
+      return () => clearInterval(interval);
+    };
+  }, []);
+
+  return timeAgoLabel;
+};
+
