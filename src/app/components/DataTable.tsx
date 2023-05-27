6955a397905b20eff3232e633fed5caac9436fb0
supramaxis <kkssccd@gmail.com>
2023-05-27 01:47:06 +0000

    Added a new table and its logic

---
+++ src/app/components/DataTable.tsx
@@ -0,0 +1,152 @@
+// DataTable.tsx
+
+'use client';
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  ColumnFiltersState,
+  getFilteredRowModel,
+  getSortedRowModel
+} from '@tanstack/react-table';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { useEffect, useMemo, useState } from 'react';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+
+export function DataTable<Tdata, TValue>({
+  columns,
+  data
+}: DataTableProps<Tdata, TValue>) {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  const memoizedData = useMemo(() => data, [data]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+  const table = useReactTable({
+    columns,
+    data: memoizedData,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      columnFilters
+    },
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    table.getColumn('shortUrl')?.setFilterValue(e.target.value);
+  }
+
+  const filterInputValue = useMemo(() => {
+    return (table.getColumn('shortUrl')?.getFilterValue() as string) ?? '';
+  }, [table]);
+
+
+  return (
+    <>
+      <div className='flex items-center py-4 pointer-events-none'>
+        <Input
+          placeholder='Filter URLS'
+          value={filterInputValue}
+          onChange={handleInputChange}
+          className='max-w-sm pointer-events-auto'
+        />
+      </div>
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className='h-24 text-center'>
+                  Sin resultados
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+
+      <div className='flex items-center justify-end space-x-2 py-4'>
+        <Button
+          variant='outline'
+          size='sm'
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}>
+          Anterior
+        </Button>
+        <Button
+          variant='outline'
+          size='sm'
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}>
+          Siguiente
+        </Button>
+      </div>
+    </>
+  );
+}
+
