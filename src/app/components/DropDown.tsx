6abdaf1c63d90451aec30d164440f6ca216dc91a
supramaxis <kkssccd@gmail.com>
2023-05-27 01:47:25 +0000

    make dropdown use swr

--- src/app/components/DropDown.tsx
+++ src/app/components/DropDown.tsx
@@ -11,6 +11,9 @@ import { HamburgerIcon, DeleteIcon, EditIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
+import { useSWRConfig } from 'swr';
+import { useContext } from 'react';
+import { UrlsContext } from '@/context/UrlsContext';
 
 interface DropDownProps {
   id: number;
@@ -25,17 +28,23 @@ const DropDown: React.FC<DropDownProps> = ({
   onUrlDeleted: () => void;
 }) => {
   const [loading, setLoading] = useState(false);
+  const { mutate } = useSWRConfig();
+  const { data, error } = useContext(UrlsContext);
 
   const handleDelete = async () => {
     setLoading(true);
 
     try {
       const res = await axios.delete(`/api/delete/${id}`);
-      
+      console.log(res.data);
       onUrlDeleted();
       toast.success('La Url ha sido eliminada');
-    } catch (error) {
-      console.log(error);
+      mutate('/api/urls');
+    } catch (error: any) {
+      console.log(error.message);
+      if (error.response) {
+        toast.error(error.response.data);
+      }
     }
     setLoading(false);
   };
