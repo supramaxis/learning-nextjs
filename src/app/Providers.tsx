9492cd301693418199f587e07319760e0f1aa426
SupraMaxis <kkssccd@gmail.com>
2023-08-08 20:13:08 -0600

    removed replaced chakra ui with shadcn ui

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,14 +1,6 @@
 // app/providers.tsx
-'use client';
-
-import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider } from '@chakra-ui/react';
+"use client";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
-    </CacheProvider>
-  );
+  return { children };
 }
-
