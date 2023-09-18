b9e42fd08a8deb61314e063e8a54602578a253ad
SupraMaxis <kkssccd@gmail.com>
2023-09-18 07:17:38 -0600

    replaced next-auth code with clerk code for getting user session

--- src/app/api/urls/route.ts
+++ src/app/api/urls/route.ts
@@ -1,21 +1,26 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prismadb';
 import getCurrentUser from '@/actions/getCurrentUser';
+import { currentUser, useAuth, clerkClient } from '@clerk/nextjs';
 
 export async function GET() {
   try {
-    const currentUser = await getCurrentUser();
-    if (!currentUser?.id || !currentUser?.email) {
-      return new NextResponse('No autorizado', { status: 401 });
-    }
-    const userId = currentUser.id;
-    const urls = await prisma.url.findMany({
-      where: {
-        userId
-      }
-    });
 
-    return NextResponse.json(urls);
+    
+    
+    // const currentUser = await getCurrentUser();
+    const actualUser = await currentUser()
+    if (!actualUser?.id || !actualUser?.primaryEmailAddressId) {
+      return new NextResponse('No autorizado', { status: 401 });
+    } else {
+          const userId = actualUser.id;
+          const urls = await prisma.url.findMany({
+            where: {
+              userId,
+            },
+          });
+          return NextResponse.json(urls);
+    }
   } catch (error) {
     console.log('ERROR_MESSAGE FROM API/URLS', error);
     return new NextResponse('ERROR_MESSAGE FROM API/URLS', { status: 500 });
