338353718767f57c6d7c30f0d616187b5636f62b
SupraMaxis <kkssccd@gmail.com>
2023-09-20 04:17:24 -0600

    replaced next-auth with clerk

--- src/app/api/delete/[id]/route.ts
+++ src/app/api/delete/[id]/route.ts
@@ -1,16 +1,15 @@
-import { NextRequest, NextResponse } from 'next/server';
-import prisma from '@/lib/prismadb';
-import { currentUser, useAuth, clerkClient } from '@clerk/nextjs';
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prismadb";
+import { currentUser, useAuth, clerkClient } from "@clerk/nextjs";
 export async function DELETE(
   request: Request,
   { params }: { params: { id: number } }
 ) {
-
   try {
-    const actualUser = await currentUser()
+    const actualUser = await currentUser();
 
-    if (!actualUser?.id || !actualUser?.externalId) {
-      return new NextResponse('No autorizado', { status: 401 });
+    if (!actualUser?.id || !actualUser?.emailAddresses?.[0]?.emailAddress) {
+      return new NextResponse("No autorizado", { status: 401 });
     }
     const urlId = Number(params.id);
     const userId = String(actualUser.id);
@@ -19,20 +18,19 @@ export async function DELETE(
       where: {
         userId_id: {
           userId,
-          id: urlId
-        }
-      }
+          id: urlId,
+        },
+      },
     });
 
     return new NextResponse(null, { status: 204 });
   } catch (error: any) {
-    if (error.code === 'P2025') {
-      return new NextResponse('No se encontro una url con ese id', {
-        status: 404
+    if (error.code === "P2025") {
+      return new NextResponse("No se encontro una url con ese id", {
+        status: 404,
       });
     }
-    console.log('ERROR_MESSAGE FROM API/URLS', error);
-    return new NextResponse('ERROR_MESSAGE FROM API/URLS', { status: 500 });
+    console.log("ERROR_MESSAGE FROM API/URLS", error);
+    return new NextResponse("ERROR_MESSAGE FROM API/URLS", { status: 500 });
   }
 }
-
