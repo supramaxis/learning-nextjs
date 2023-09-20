f74ffd6a019c522550fc1a6bf860ced8ae415716
SupraMaxis <kkssccd@gmail.com>
2023-09-20 16:51:05 -0600

    formatting

--- src/app/components/TableButton/Button.tsx
+++ src/app/components/TableButton/Button.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -8,12 +8,13 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ children, className, ...rest }: ButtonProps) {
   return (
     <button
-      type='button'
+      type="button"
       className={cn(
-        'relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50',
-        className || ''
+        "relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50",
+        className || ""
       )}
-      {...rest}>
+      {...rest}
+    >
       {children}
     </button>
   );
@@ -22,14 +23,14 @@ export function Button({ children, className, ...rest }: ButtonProps) {
 export function PageButton({ children, className, ...rest }: ButtonProps) {
   return (
     <button
-      type='button'
+      type="button"
       className={cn(
-        'relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50',
-        className || ''
+        "relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50",
+        className || ""
       )}
-      {...rest}>
+      {...rest}
+    >
       {children}
     </button>
   );
 }
-
