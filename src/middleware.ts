4f6cbe637fe6f31ea66ce4a0f78902ca79d57647
SupraMaxis <kkssccd@gmail.com>
2023-09-22 01:38:21 -0600

    modified clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,18 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/login", "/register"],
+  publicRoutes: [
+    "/login",
+    "/register",
+    "/api/webhooks/users",
+    "/api/urls",
+    "/",
+  ],
+  afterAuth(auth, req, evt) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
 });
 
 export const config = {
