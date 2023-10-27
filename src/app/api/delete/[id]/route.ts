fe9f3df5817a3a1f0b0c02ce77afd54b71396934
SupraMaxis <kkssccd@gmail.com>
2023-10-27 17:43:18 -0600

    debugging

--- src/app/api/delete/[id]/route.ts
+++ src/app/api/delete/[id]/route.ts
@@ -8,6 +8,8 @@ import type { NextApiRequest } from "next";
 export async function DELETE(req: NextApiRequest) {
   const url = new URL(req.url!, "http://localhost:3000");
   const linkIdstr = url.pathname.split("/").pop();
+  console.log(url);
+  console.log(linkIdstr);
   const { sessionId } = auth();
   const actualUser = await currentUser();
 
