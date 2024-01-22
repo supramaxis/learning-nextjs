222f78e1e1f85d2d74ad0e0fa95c6bc28a41ef58
SupraMaxis <kkssccd@gmail.com>
2024-01-21 23:04:17 -0600

    Add a demo API endpoint to generate short URLs using Math.random()

---
+++ src/app/api/demo/route.ts
@@ -0,0 +1,26 @@
+//api endpoint to get a short url from a long url using prisma posgresql database
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { url } = body;
+  const slug = Math.random().toString(36).substring(2, 7);
+
+  
+
+  try {
+    const data = {
+      url,
+      slug,
+    };
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error, {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
