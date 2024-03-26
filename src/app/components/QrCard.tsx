7f1d427210d9e2c881c05901b51931161c796db4
SupraMaxis <kkssccd@gmail.com>
2024-03-26 02:06:37 -0600

    added qrcard component to the project.

---
+++ src/app/components/QrCard.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+const QrCard = ({ qrCodeLink }: { qrCodeLink: string }) => {
+  const [showDialog, setShowDialog] = useState(false);
+
+  const handleOpenDialog = () => {
+    setShowDialog(true);
+  };
+
+  return (
+    <>
+      <Dialog defaultOpen>
+        {/* <DialogTrigger asChild>
+            <Button onClick={handleOpenDialog}>Generate QR Code</Button>
+        </DialogTrigger> */}
+        {/* {showDialog && ( */}
+        <DialogContent className="sm:max-w-[320px]">
+          <DialogHeader>
+            <div className="flex justify-center items-center ">
+              <DialogTitle>Your QR Code</DialogTitle>
+            </div>
+            <div className="flex justify-center items-center ">
+              <DialogDescription>
+                This is your generated QR Code
+              </DialogDescription>
+            </div>
+          </DialogHeader>
+
+          <div className="flex justify-center items-center ">
+            <div className="grid gap-4">
+              <Image src={qrCodeLink} width={150} height={150} alt="QR Code" />
+            </div>
+          </div>
+          <DialogFooter></DialogFooter>
+        </DialogContent>
+
+        {/* )} */}
+      </Dialog>
+    </>
+  );
+};
+
+export default QrCard;
