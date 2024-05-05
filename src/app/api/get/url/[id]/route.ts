97a0182b9577677fed11d99fdbb1b4623a3cad89
SupraMaxis <kkssccd@gmail.com>
2024-05-05 01:40:59 -0600

    moved route handler logic into its own method using URLHandler

--- src/app/api/get/url/[id]/route.ts
+++ src/app/api/get/url/[id]/route.ts
@@ -1,37 +1,7 @@
-import prisma from "@/lib/prismadb";
-import { NextResponse, NextRequest } from "next/server";
-import { currentUser } from "@clerk/nextjs";
-import { clerkClient } from "@clerk/nextjs/server";
-import { auth } from "@clerk/nextjs";
+import { URLHandler } from "@/classes/URLHandler";
 import type { NextApiRequest } from "next";
 
-export async function GET(req: NextApiRequest) {
-  const url = new URL(req.url!, "http://localhost:3000");
-  const linkIdstr = url.pathname.split("/").pop();
-  // console.log(url);
-  console.log(linkIdstr);
-  const { sessionId } = auth();
-  const actualUser = await currentUser();
-
-  const session = sessionId
-    ? await clerkClient.sessions.getSession(sessionId)
-    : null;
-  const userId = session?.userId;
-
-  try {
-    if (!userId || !actualUser?.emailAddresses?.[0]?.emailAddress) return new NextResponse("Unauthorized", { status: 401 });
-
-    const singleUrl = await prisma.url.findFirst({
-      where: {
-        id: linkIdstr,
-      },
-    });
-    // console.log(null, { status: 204 });
-    return NextResponse.json(singleUrl?.url, {status: 200});
-  } catch (error: any) {
-    if (error.code === "P2025") return new NextResponse("URL not found", {status: 404});
-    
-    console.log("delete endpoint error:", error);
-    return new NextResponse("delete endpoint error:" + error, { status: 500 });
-  }
+export async function GET(req: Request) {
+  const handler = new URLHandler(req);
+  return handler.getURL();
 }
--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -1,90 +1,9 @@
 //api endpoint to get a short url from a long url using prisma posgresql database
 
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prismadb";
-import { clerkClient } from "@clerk/nextjs/server";
-import { auth } from "@clerk/nextjs";
+import { URLHandler } from "@/classes/URLHandler";
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const { url, customCode } = body;
-  const { sessionId } = auth();
-  const session = sessionId
-    ? await clerkClient.sessions.getSession(sessionId)
-    : null;
-  console.log(`Session: ${JSON.stringify(session)}`);
-  let shortUrl = customCode;
-  const userId = session?.userId;
-
-  if (!userId) {
-    return (
-      NextResponse.json({ error: "Unauthorized" }),
-      {
-        status: 401,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  }
-
-  if (!shortUrl) shortUrl = Math.random().toString(36).substring(2, 7);
-
-  try {
-    if (!userId) {
-      return NextResponse.json(
-        { error: "No user found" },
-        { status: 404, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    if (session) {
-      const userId = session.userId;
-      if (userId) {
-        if (customCode) {
-          const existingUrl = await prisma.url.findFirst({
-            where: {shortUrl: customCode}
-          })
-
-          if (existingUrl) {
-            return NextResponse.json({error: {
-              message: "Custom code already exists"
-            }}, {
-              status: 409,
-              headers: {"Content-Type": "application/json"}
-            })
-          }
-        }
-
-
-        const data = await prisma.url.create({
-          data: {
-            url,
-            shortUrl,
-            user: {
-              connect: {
-                externalId: session.userId,
-              },
-            },
-          },
-        });
-        console.log(data);
-        return NextResponse.json(data);
-      } else {
-        return NextResponse.json(
-          { error: "userId null or undefined" },
-          { status: 500, headers: { "Content-Type": "application/json" } }
-        );
-      }
-    } else {
-      return NextResponse.json(
-        { error: "No session found" },
-        { status: 401, headers: { "Content-Type": "application/json" } }
-      );
-    }
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(error, {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+export async function POST(req: Request) {
+  const handler = new URLHandler(req);
+  const res = await handler.createShortUrl();
+  return res;
 }
